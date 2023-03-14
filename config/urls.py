2ae062a8c9ca13a7a82edf712533ab66447aff0c
Loic Ho-Von <loic.hovon@gmail.com>
2023-03-14 14:31:58 -0400

    Merge latest activity votes and note, favicon hack

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('buildings.urls')),
-    # path("favicon.ico", RedirectView.as_view(url='/static/images/favicon.ico')), # hack to get favicon in dev
+    path("favicon.ico", RedirectView.as_view(url='/static/images/favicon.ico')), # hack to get favicon - how to do this properly??
 ]
