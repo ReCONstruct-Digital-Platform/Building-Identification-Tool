36ebcda5859ce9a481982d649ac81e5f70f43d29
Loic Ho-Von <loic.hovon@gmail.com>
2023-08-17 17:11:57 -0400

    Add Time Travel to Streetview! Remove Google logo from screenshots, Cleanup CSS

--- buildings/static/scripts/init_gmaps.js
+++ buildings/static/scripts/init_gmaps.js
@@ -1,5 +1,70 @@
-// Retrieve some data passed to the script by the template
+// Retrieve some data passed to the script by django template
 var data = document.currentScript.dataset;
+
+
+function timeTravel(panoId) {
+  window.sv.setPano(panoId);
+}
+
+/*
+* Take an array of available panoramas and their dates
+* and return a list of <option> elements for a drop-down
+*/
+function generateSelectOptions(panoArray, selectedDate) {
+  const options = [];
+  const dateSplit = selectedDate.split('-');
+
+  const selectedPanoDate = 
+    new Date(Date.UTC(dateSplit[0], dateSplit[1], 1))
+      .toLocaleDateString('en-US', { year:"numeric", month:"long"});
+  
+  /* 
+  * Get the date key.
+  * Assumptions:
+  * - All elements of the array have the same keys
+  * - The date key immediately follows the panorama ID key
+  * In practice, always observed 2 keys per element
+  * and both 'Qm' and 'yp' as date keys. No idea if there are others.
+  */
+  const dateKey = Object.keys(panoArray[0]).filter((e) => {return e !== 'pano'})[0];
+
+  panoArray.reverse().forEach((el, i) => {
+    
+    let option = document.createElement('option');
+    option.value = el['pano'];
+    
+    const date = new Date(el[dateKey]).toLocaleDateString('en-US', { year:"numeric", month:"long"});
+    option.innerText = date;
+    
+    if (!date) {
+      console.log('Could not get date from element: ', el);
+    }
+    
+    // Match the dates as strings
+    // I couldn't find how to declare a Date as UTC easily
+    // Format is 'full-month year'
+    if (date === selectedPanoDate) {
+      option.selected = true;
+    }
+    options.push(option);
+  });
+  return options;
+}
+
+
+/*
+* Function to hide certain things on the streetview as they get loaded in
+* You can console.log() all the mutations first to know which ones to select.
+*/
+function mutationCallbackHideGoogleLogo(mutationList, _) {
+  for (const mutation of mutationList) {
+    if (
+      mutation.target.getAttribute('title') === "Open this area in Google Maps (opens a new window)" 
+    ) {
+        mutation.target.style.display = "none";
+    }
+  }
+};
 
 
 function findPanorama(svService, latestViewData, panoRequest, evalUnitCoord) {
@@ -8,7 +73,8 @@ function findPanorama(svService, latestViewData, panoRequest, evalUnitCoord) {
   // Send a request to the panorama service
   svService.getPanorama(panoRequest, function(panoData, status) 
   {
-    if (status === google.maps.StreetViewStatus.OK) {
+    if (status === google.maps.StreetViewStatus.OK) 
+    {
       console.log(`Status ${status}: panorama found.`);
 
       let heading, zoom, pitch;
@@ -33,8 +99,10 @@ function findPanorama(svService, latestViewData, panoRequest, evalUnitCoord) {
               heading: heading,
               pitch: pitch,
             },
+            imageDateControl: true,
+            fullscreenControl: false,
         });
-
+      
       const sv_marker = new google.maps.Marker({
         position: evalUnitCoord,
         map: sv,
@@ -51,9 +119,8 @@ function findPanorama(svService, latestViewData, panoRequest, evalUnitCoord) {
         center: evalUnitCoord,
         mapTypeId: 'satellite',
         zoom: 18,
-        // streetViewControl: false,
-        rotateControl: false,
         controlSize: 25,
+        fullscreenControl: false,
       });
       
       const m_marker = new google.maps.Marker({
@@ -69,6 +136,22 @@ function findPanorama(svService, latestViewData, panoRequest, evalUnitCoord) {
       window.map = map;
       window.sv_marker = sv_marker;
       window.m_marker = m_marker;
+      
+      // Set the time travel select visible only once the streetview is loaded
+      // Otherwise it shows on the side of the screen before ending in the right place
+      google.maps.event.addListenerOnce(map, 'idle', () => {
+        const options = generateSelectOptions(panoData.time, panoData.imageDate);
+        document.getElementById('time-travel-select').append(...options);
+        document.getElementById('time-travel-container').style.display = 'flex';
+      });
+
+      
+      // Register a mutation observer to filter out elements
+      const observer = new MutationObserver(mutationCallbackHideGoogleLogo);
+      const config = { attributes: true, childList: true, subtree: true };
+      observer.observe(document.getElementById('streetview'), config);
+      observer.observe(document.getElementById('satmap'), config);
+
 
       // Set ondrag event listeners for both markers
       // Dragging any of them will instantly update the other
@@ -150,8 +233,10 @@ function initMaps() {
     };
   }
 
-  return findPanorama(svService, latestViewData, panoRequest, evalUnitCoord);
+  findPanorama(svService, latestViewData, panoRequest, evalUnitCoord);
 }
+
+
 
 window.initMaps = initMaps;
 
@@ -163,4 +248,3 @@ var script = document.createElement('script');
 script.src = gmapsURL;
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
-
