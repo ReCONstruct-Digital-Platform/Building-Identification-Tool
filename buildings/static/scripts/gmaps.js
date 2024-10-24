06a2160da93f685408eff7f98c058823fa5a6b47
unknown <loic.hovon@gmail.com>
2024-10-24 16:29:58 -0400

    Fix timetravel pegman drag-drop for newest gmaps version

--- buildings/static/scripts/gmaps.js
+++ buildings/static/scripts/gmaps.js
@@ -29,7 +29,7 @@ const dataset = document.currentScript.dataset;
 
     if (!evalUnitCoord)
       console.debug('No eval unit coords found')
-    
+
     panoRequest = {
       location: evalUnitCoord,
       preference: google.maps.StreetViewPreference.NEAREST,
@@ -97,6 +97,8 @@ function generateTimeTravelOptions(panoArray, targetDate) {
   // Set the minimum difference element to selected
   closestPanoEl.selected = true;
 
+  // console.debug(`Closest pano elem: ${closestPanoEl.value} with date ${closestPanoDate}`)
+
   return {
     options,
     closestPanoId: closestPanoEl.value,
@@ -112,29 +114,60 @@ function generateTimeTravelOptions(panoArray, targetDate) {
 function attachEventsToPegman(mutationList, _) {
   for (const mutation of mutationList) {
     if (
-      mutation.target.getAttribute("src") ===
-      "https://maps.gstatic.com/mapfiles/transparent.png"
+      mutation.target.getAttribute("class") === "yNHHyP-marker-view"
     ) {
-      // Without this, we easily get hundreds of listeners attached
-      if (mutation.target.getAttribute("listenersSet")) return;
+      if (mutation.type === "attributes" && mutation.attributeName === "style") return;
 
-      console.debug("Attaching events on pegman");
-      // Save a reference if needed later
-      window.pegman = mutation.target;
-
-      // Add event listeners to the element
-      mutation.target.addEventListener("mousedown", () => {
-        window.pegmanDropped = false;
-        window.pegmanMousedown = true;
-      });
-      mutation.target.addEventListener("mouseup", () => {
-        if (window.pegmanMousedown) {
-          console.debug('pegman dropped');
-          window.pegmanMousedown = false;
-          window.pegmanDropped = true;
+      if (mutation.type === "attributes" && mutation.attributeName === "aria-grabbed") {
+        // https://stackoverflow.com/questions/11290007/is-mutationrecord-oldvalue-something-i-should-be-using
+        // If oldValue of aria-grabbed was true, it means element was grabbed and is not released
+        // hence the pegman just got dropped! Else if oldValue was false, then it was not grabbed and is now being grabbed.
+        if (mutation.oldValue === "false") {
+          if (!window.pegmanDragInitiated) {
+            console.debug("pegman drag initiated")
+            window.pegmanDropped = false;
+            window.pegmanDragInitiated = true;
+          }
+          else {
+            console.debug("pegman drag ended")
+            window.pegmanDragInitiated = false;
+            window.pegmanDropped = true;
+          }
         }
-      });
-      mutation.target.setAttribute("listenersSet", "true");
+        // else if (mutation.oldValue === "true") {
+        //   console.debug("pegman drag ended 1")
+        //   window.pegmanDropped = true;
+        // }
+      }
+
+      //   // Without this, we easily get hundreds of listeners attached
+      //   if (mutation.target.getAttribute("listenersSet") === "true") {
+      //     console.debug("pegman event listeners already set")
+      //     return;
+      //   }
+      //   console.debug("Attaching events on pegman");
+      //   // Save a reference for later
+      //   window.pegman = mutation.target.getElementsByTagName('img')[0]
+      //   window.pegmanDiv = mutation.target;
+
+      //   // console.log(window.pegman)
+
+      //   // Add event listeners to the element
+      //   window.pegman.addEventListener("mousedown", () => {
+      //     console.debug("mousedown on pegman")
+      //     window.pegmanDropped = false;
+      //     window.pegmanMousedown = true;
+      //   });
+      //   window.pegman.addEventListener("mouseup", () => {
+      //     console.debug("mouseup on pegman");
+
+      //     if (window.pegmanMousedown) {
+      //       console.debug('pegman dropped');
+      //       window.pegmanMousedown = false;
+      //       window.pegmanDropped = true;
+      //     }
+      //   });
+      //   mutation.target.setAttribute("listenersSet", "true");
     }
   }
 }
@@ -158,34 +191,34 @@ const selectedLot = {
 
 function onClick(e) {
   if (e.feature.getProperty('clicked') === 'true') {
-      e.feature.setProperty('clicked', 'false');
-      deletePoints(e.feature);
+    e.feature.setProperty('clicked', 'false');
+    deletePoints(e.feature);
   }
   else {
-      // If another feature is currently clicked
-      if (Object.keys(selectedLot.feature).length) {
-          selectedLot.feature.setProperty('clicked', 'false');
-          deletePoints(selectedLot.feature);
-      }
-      e.feature.setProperty('clicked', 'true');
-      showLotPoints(e.feature);
-      selectedLot.feature = e.feature;
+    // If another feature is currently clicked
+    if (Object.keys(selectedLot.feature).length) {
+      selectedLot.feature.setProperty('clicked', 'false');
+      deletePoints(selectedLot.feature);
+    }
+    e.feature.setProperty('clicked', 'true');
+    showLotPoints(e.feature);
+    selectedLot.feature = e.feature;
   }
 }
 
 function styleFeatures(feature) {
 
   return {
-      fillColor: "#55eb34",
-      fillOpacity: 0.35,
-      strokeColor: "#2dbf0d",
-      strokeWeight: 1,
+    fillColor: "#55eb34",
+    fillOpacity: 0.35,
+    strokeColor: "#2dbf0d",
+    strokeWeight: 1,
   }
 }
 
 async function deletePoints(feature) {
   feature.getProperty('points').forEach((marker) => {
-      marker.setMap(null);
+    marker.setMap(null);
   })
   feature.getProperty('points').length = 0;
 }
@@ -194,31 +227,31 @@ async function showLotPoints(feature) {
   const { Marker } = await google.maps.importLibrary("marker");
 
   const svgMarker = {
-      path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-      fillColor: "#55eb34",
-      strokeColor: "#2dbf0d",
-      fillOpacity: 0.8,
-      strokeWeight: 1,
-      rotation: 0,
-      scale: 4,
-    };
+    path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+    fillColor: "#55eb34",
+    strokeColor: "#2dbf0d",
+    fillOpacity: 0.8,
+    strokeWeight: 1,
+    rotation: 0,
+    scale: 4,
+  };
 
   if (!feature.getProperty('points')) {
-      feature.setProperty('points', []);
+    feature.setProperty('points', []);
   }
 
   const addPoint = (coords) => {
-      console.debug(`creating point at ${coords}`)
-      feature.getProperty('points').push(new Marker({
-          position: coords,
-          map: window.map,
-          icon: svgMarker,
-      }))
-      feature.getProperty('points').push(new Marker({
-          position: coords,
-          map: window.sv,
-          icon: svgMarker,
-      }))
+    console.debug(`creating point at ${coords}`)
+    feature.getProperty('points').push(new Marker({
+      position: coords,
+      map: window.map,
+      icon: svgMarker,
+    }))
+    feature.getProperty('points').push(new Marker({
+      position: coords,
+      map: window.sv,
+      icon: svgMarker,
+    }))
   }
   feature.getGeometry().forEachLatLng(addPoint);
 }
@@ -239,8 +272,10 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
         console.debug(`Status ${status}: panorama found within ${panoRequest.radius}m`);
       }
       else {
-        console.debug(`Status ${status}: panorama ${panoRequest.pano} found`);
+        console.debug(`Status ${status}: panorama ${panoRequest.pano} found by ID`);
       }
+
+      // console.debug(`Data: ${JSON.stringify(data, null, 2)}`)
 
       let heading;
       let zoom = pitch = 0;
@@ -270,7 +305,7 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
         }
       );
       sv.setPano(data.location.pano);
-      
+
       const map = new Map(document.getElementById("satellite"), {
         center: evalUnitCoord,
         mapTypeId: "hybrid",
@@ -329,8 +364,8 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
 
       const lotShape = JSON.parse(document.getElementById("geojson").textContent);
       console.debug(lotShape);
-      
-        // load the data
+
+      // load the data
       map.data.addGeoJson(lotShape);
       map.data.setStyle(styleFeatures)
       map.data.addListener("mouseover", e => e.feature.setProperty("state", "hover"));
@@ -338,7 +373,7 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
       map.data.forEach(showLotPoints);
 
       // map.data.addListener("click", onClick);
-      
+
       // Generate the initial time travel dropdown
       const { options } = generateTimeTravelOptions(data.time, data.imageDate);
       // Set the time travel select visible only once the streetview is loaded
@@ -348,45 +383,91 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
 
       // Register a mutation observer to attach events to the pegman
       const observer = new MutationObserver(attachEventsToPegman);
-      const config = { attributes: true, subtree: true };
+      const config = { attributes: true, subtree: true, attributeOldValue: true };
       observer.observe(document.getElementById("satellite"), config);
+
+      // https://developers.google.com/maps/documentation/javascript/reference/street-view#StreetViewPanorama.status_changed
+      sv.addListener("status_changed", () => {
+        console.debug(`status_changed:
+  newPanoId: 
+  newPanoDate: 
+  window.shouldBePano: ${window.shouldBePano}
+  window.shouldBePanoDate: ${window.shouldBePanoDate}
+  window.lastPanoId: ${window.lastPanoId}
+  window.lastPanoDate: ${window.lastPanoDate}
+  window.pegmanDropped: ${window.pegmanDropped}
+  window.lastManualPanoChangeDone: ${JSON.stringify(window.lastManualPanoChangeDone)}
+        `)
+        if (window.shouldBePano && sv.getPano() != window.shouldBePano) {
+          console.debug(`window.shouldBePano && sv.getPano() != window.shouldBePano - setting pano again`)
+          sv.setPano(window.shouldBePano)
+        }
+        // Finally good state after a pegman drag-drop. Reset shouldBePano.
+        else if (
+          window.shouldBePano === window.lastPanoId
+        ) {
+          window.shouldBePano = undefined
+        }
+      });
 
       // Custom event launched when pegman is dropped and we need a manual pano set
       sv.addListener("pano_change_needed", () => {
         sleep(0).then(() => {
+          console.debug(`Manually changing pano to ${window.shouldBePano} (${window.shouldBePanoDate})`);
+          window.lastManualPanoChangeDone = {
+            from: sv.getPano(),
+            to: window.shouldBePano
+          }
           sv.setPano(window.shouldBePano);
-          console.debug(`Changed pano to ${window.shouldBePano}`);
         });
       });
 
+      // This event is triggered by gmaps automatically when the panorama changes
+      // https://developers.google.com/maps/documentation/javascript/reference/street-view#StreetViewPanorama.pano_changed
       sv.addListener("pano_changed", () => {
-        console.debug("pano_changed");
-
         const newPanoId = sv.getPano();
 
-        // Detect when we need to change the panorama after a pegman drop
+        // Detect when we manually triggered a panorama change after a pegman drop
         if (window.panoChangeNeeded) {
           window.panoChangeNeeded = false;
-          console.debug(`Starting change to ${window.shouldBePano}`);
-          // trigger custom event
-          return event.trigger(sv, "pano_change_needed");
+          console.debug(`Manually triggered pano change`);
+          return
         }
+
+
         // Skip duplicate events
         if (window.lastPanoId === newPanoId) {
           console.debug(`Extra event on ${window.lastPanoId}, returning!`);
           return;
         }
+
+        if (window.lastManualPanoChangeDone &&
+          window.lastManualPanoChangeDone.from === newPanoId &&
+          window.lastManualPanoChangeDone.to === window.shouldBePano
+        ) {
+          console.debug("trying to undo manual pano change!")
+          return;
+        }
+
         // Get more info on the pano from StreetViewService
         svService.getPanorama({ pano: newPanoId }, (data, status) => {
           if (status === StreetViewStatus.OK) {
-            console.debug(`New pano: ${newPanoId} - (${data.imageDate})`);
-            console.debug(`Last pano: ${window.lastPanoId} - (${window.lastPanoDate})`);
+            // console.debug(`svService called with panoId ${newPanoId}`)
+            console.debug(`pano_changed svService return OK:
+  newPanoId: ${newPanoId}
+  newPanoDate: ${data.imageDate}
+  window.shouldBePano: ${window.shouldBePano}
+  window.shouldBePanoDate: ${window.shouldBePanoDate}
+  window.lastPanoId: ${window.lastPanoId}
+  window.lastPanoDate: ${window.lastPanoDate}
+  window.pegmanDropped: ${window.pegmanDropped}
+  window.lastManualPanoChangeDone: ${JSON.stringify(window.lastManualPanoChangeDone)}
+            `)
 
             // If the pegman was just dropped and the new panorama's date is not equal
             // to the last panorama's date, we manually change the panorama to the
             // one closest in time to the pre-pegman drop date.
-            if (window.pegmanDropped && data.imageDate !== window.lastPanoDate)
-            {
+            if (window.pegmanDropped && data.imageDate !== window.lastPanoDate) {
               window.pegmanDropped = false;
               console.debug("Pegman dropped and new pano date not equal to last.");
 
@@ -397,16 +478,18 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
               // Set this variable so we know we need to change the pano
               window.panoChangeNeeded = true;
               window.shouldBePano = closestPanoId;
+              window.shouldBePanoDate = closestPanoDate;
+
+              event.trigger(sv, "pano_change_needed")
               return;
             }
-            console.debug(`Generating dropdown options for new pano`);
 
             const { options } = generateTimeTravelOptions(
               data.time,
               data.imageDate
             );
             document.getElementById("time-travel-select")
-                    .replaceChildren(...options);
+              .replaceChildren(...options);
 
             // save the current pano date for next time
             window.lastPanoDate = data.imageDate;
@@ -414,8 +497,6 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
           }
         });
       });
-
-
     }
     // Check if we were doing a radius search
     else if (panoRequest.radius) {
@@ -445,7 +526,7 @@ async function findPanorama(svService, latestViewData, panoRequest, evalUnitCoor
     // Else we were doing an ID search - switch to radius search
     else {
       console.debug(`Could not find pano ${panoRequest.pano}. Switching to radius search.`);
-      
+
       // Making a radius search request
       panoRequest = {
         location: evalUnitCoord,
