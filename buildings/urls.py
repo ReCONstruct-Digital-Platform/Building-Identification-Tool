f20b0e7d0b1a3b5a0d2832c45e71b3db9cec8b41
unknown <loic.hovon@gmail.com>
2024-10-25 14:03:33 -0400

    New cicd workflow

--- buildings/urls.py
+++ buildings/urls.py
@@ -13,20 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from . import views
 
 # This sets the application namespace
-app_name = 'buildings'
+app_name = "buildings"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('all_buildings', views.all_buildings, name='all_buildings'),
-    path('survey', views.survey, name="survey"),
-    path('survey/v1/<str:eval_unit_id>', views.survey_v1, name="survey_v1"),
-    path('register', views.register, name="register"),
-    path('login', views.login_page, name="login"),
-    path('logout', views.logout_page, name="logout"),
-    path('upload_imgs/<str:eval_unit_id>', views.upload_imgs, name="upload_imgs"),
-    path('redeploy_server', views.redeploy_server, name="redeploy_server"),
+    path("", views.index, name="index"),
+    path("all_buildings", views.all_buildings, name="all_buildings"),
+    path("survey", views.survey, name="survey"),
+    path("survey/v1/<str:eval_unit_id>", views.survey_v1, name="survey_v1"),
+    path("register", views.register, name="register"),
+    path("login", views.login_page, name="login"),
+    path("logout", views.logout_page, name="logout"),
+    path("upload_imgs/<str:eval_unit_id>", views.upload_imgs, name="upload_imgs"),
 ]
