4e7fda576f5d814ef064ff9bbe9dff633c208668
Loic Ho-Von <loic.hovon@gmail.com>
2025-02-01 08:44:32 -0500

    Modify scripts for staging 2

--- buildings/management/commands/new_version_create_metal_dataset_and_buildings.py
+++ buildings/management/commands/new_version_create_metal_dataset_and_buildings.py
@@ -35,11 +35,11 @@ SQL_UPSERT_BUILDING_TEMPLATE = f"""(%(ext_id)s, %(lat)s, %(lng)s, %(point)s, %(d
 def upsert_evalunits(dry_run=True):
 
     from_db = psycopg2.connect(
-        user="bitdbuser",
-        password="new_password",
-        host="127.0.0.1",
-        port=5432,
-        database="bitdb4",
+        user=ENV["POSTGRES_USER"],
+        password=ENV["POSTGRES_PW"],
+        database=ENV["POSTGRES_NAME"],
+        port=ENV["POSTGRES_PORT"],
+        host=ENV["POSTGRES_HOST"],
     )
     from_cur = from_db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
@@ -47,7 +47,8 @@ def upsert_evalunits(dry_run=True):
         user=ENV["POSTGRES_USER"],
         password=ENV["POSTGRES_PW"],
         database=ENV["POSTGRES_NAME"],
-        port=5433,
+        port=ENV["POSTGRES_PORT"],
+        host=ENV["POSTGRES_HOST"],
     )
     to_cur = to_db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
