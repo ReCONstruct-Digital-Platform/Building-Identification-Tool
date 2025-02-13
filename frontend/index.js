0afee60996f9df098987283e59915c3355234f7f
Loic Ho-Von <loic.hovon@gmail.com>
2025-02-13 09:03:34 -0500

    Typescript support

--- frontend/index.js
+++
@@ -1,6 +0,0 @@
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = "Hello webpack!! super awesome HMR";
-  return element;
-}
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./frontend/index.js", // path to our input file
+  entry: "./frontend/index.ts", // path to our input file
   output: {
     filename: "index-bundle.js", // output bundle file name
     path: path.resolve(__dirname, "./static"), // path to our Django static directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
